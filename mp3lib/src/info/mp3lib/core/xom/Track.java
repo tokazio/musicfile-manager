f0adf12eaf9e9b4edd21483b335150021d3089a1
palito686 <palito686@f46b8487-d246-0410-87de-0759a80fe282>
2008-02-24 19:58:38 +0000

    xom UML Class diagram created via reverse engineering Mp3 class renamed to Track Track.builfTrackElementFromFileImplemented zicFile POC modified

--- mp3lib/src/info/mp3lib/core/xom/Track.java
+++ mp3lib/src/info/mp3lib/core/xom/Track.java
@@ -1,19 +1,28 @@
 package info.mp3lib.core.xom;
 
-import java.io.File;
 import java.security.InvalidParameterException;
-import java.util.HashMap;
-import java.util.Map;
 
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+
+import com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl;
+
+import entagged.audioformats.AudioFile;
+import entagged.audioformats.Tag;
+import org.apache.log4j.*;
+import org.apache.log4j.spi.LoggerFactory;
 
 /**
  * All objects corresponding to a music files in mp3 format.
  * @author Gabriel Pala
  */
-public class Mp3 extends AbstractMusicFile implements ITaggedMusicFile{
+public class Track extends AbstractMusicFile implements ITaggedMusicFile{
 	/* ------------------------ ATTRIBUTES ------------------------ */
-	
+	 private final static Logger LOGGER = Logger.getLogger(AbstractMusicFile.class.getName()); 
 	/* ----------------------- CONSTRUCTORS ----------------------- */
 	/**
 	 * Constructs a new Mp3 from the file specified.
@@ -22,7 +31,7 @@ public class Mp3 extends AbstractMusicFile implements ITaggedMusicFile{
 	 * @throws InvalidParameterException when the File given in parameters
 	 * doesn't correspond to a valid mp3 file
 	 */
-	public Mp3(File mp3File, boolean buildNode) throws InvalidParameterException {
+	public Track(AudioFile mp3File, boolean buildNode) throws InvalidParameterException {
 		super(mp3File);
 		// TODO verify the validity of argument and implement the exception mechanism
 		// if (mp3File == null || mp3File.getName() != *.mp3) => throw new InvalidParameterException
@@ -38,7 +47,7 @@ public class Mp3 extends AbstractMusicFile implements ITaggedMusicFile{
 	 * @throws InvalidParameterException when the Element given in parameters
 	 * doesn't correspond to a valid track Element
 	 */
-	public Mp3(Node node, boolean retrieveFile) throws InvalidParameterException {
+	public Track(Node node, boolean retrieveFile) throws InvalidParameterException {
 		super(node);
 		// TODO verify the validity of argument and implement the exception mechanism
 		if (retrieveFile) {
@@ -71,22 +80,39 @@ public class Mp3 extends AbstractMusicFile implements ITaggedMusicFile{
 	
 	/**
 	 * Retrieve tags from the mp3 file.
-	 * @author
-	 * @return tagMap a map binding tag name and tag value, 
+	 * @return a Tag object containing all tag information of the audioFile
 	 * return null if tags aren't available
 	 */
-	public Map<String, String> getTags() {
-		Map<String, String> tagMap =  new HashMap<String, String>();
-		// TODO method implementation
-		return tagMap;
+	public Tag getTag() {
+		return ((AudioFile)musicFile).getTag();
 	}
 	
 	/**
 	 * build the track Element from informations retrieved from the mp3 file
 	 * and set it in this.node
+	 * @throws ParserConfigurationException 
 	 */
 	private void buildTrackElementFromFile() {
-		// TODO method implementation
+		try {
+			LOGGER.debug("buildTrackElementFromFile()");
+			Document doc =  DocumentBuilderFactoryImpl.newInstance().newDocumentBuilder().newDocument();
+			Element element = doc.createElement("track");
+			element.setAttribute("code", "0");
+			element.setAttribute("currentPath", musicFile.getPath());
+			element.setAttribute("newPath", "");
+			element.setAttribute("name", getTag().getFirstTitle());
+			element.setAttribute("length",new StringBuffer().append(musicFile.length()).toString());
+			element.setAttribute("size",new StringBuffer().append(((AudioFile)musicFile).getLength()).toString());
+			node = element;
+		} catch (DOMException e) {
+			LOGGER.error(new StringBuffer("unable to build XML element : ")
+					.append(e.getMessage()).toString());
+			//e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			LOGGER.error(new StringBuffer("unable to build XML element : ")
+			.append(e.getMessage()).toString());
+			//e.printStackTrace();
+		}
 	}
 	
 	/**
