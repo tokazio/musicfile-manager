8e8f378e21730ad2cb6233149a9b9a9dd4286ccb
donatien.riviere <donatien.riviere@f46b8487-d246-0410-87de-0759a80fe282>
2008-08-18 22:09:16 +0000

    main procedure update

--- mp3lib/src/info/mp3lib/core/main/Mp3lib.java
+++ mp3lib/src/info/mp3lib/core/main/Mp3lib.java
@@ -15,38 +15,46 @@ import java.io.IOException;
 
 public class Mp3lib {
 
-	/**
-	 * @param args
-	 * 
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		Mp3lib m = new Mp3lib();
-		
-		final String musicDirectory = "E:/mp3test";
-		File musicFileDir = new File(musicDirectory); 
-		DataScanner.getInstance().read(musicFileDir);
-		LibraryDAO.getInstance().write(Library.getInstance(), "c:/zicfile.xml");
-		
-		System.out.println("Scan complete !");
-		
-		System.out.println("Validation ...");
-		
-		Library.getInstance().validate();
-		
-	}
+    /**
+     * @param args
+     * 
+     */
+    public static void main(String[] args) {
 
-	/**
-	 * Ceci est une description
-	 * 
-	 * @author Administrateur
-	 * @param s
-	 *            Nom du fichier
-	 * @return true si reussite, false sinon.
-	 * @throws IOException
-	 */
-	public String testEx(String s) throws IOException {
-		return "bla";
-	}
+	// Set entry point.
+	final String musicDirectory = "E:/mp3test";
+	// Set XML library file path
+	final String xmlLibrary = "E:/zicfile.xml";
+	
+	
+	// Launch SCAN procedure : Fill Library
+	System.out.println("Scanning directory ["+musicDirectory+"]...");
+	DataScanner.getInstance().read(new File(musicDirectory));
+	System.out.println("Scan complete !");
+	
+	// Save library to XML
+	LibraryDAO.getInstance().write(Library.getInstance(), xmlLibrary);
+	
+	// TODO: reload library from existing XML if user-requested ..
+	
+	// Launch TAG procedure : make best tag..
+	System.out.println("Validating library ...");
+	Library.getInstance().validate();
+	System.out.println("Validation complete !");
+
+    }
+
+    /**
+     * Ceci est une description
+     * 
+     * @author Administrateur
+     * @param s
+     *                Nom du fichier
+     * @return true si reussite, false sinon.
+     * @throws IOException
+     */
+    public String testEx(String s) throws IOException {
+	return "bla";
+    }
 
 }
