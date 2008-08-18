c56b034777414a293a5c643a3a8dda031187fb00
donatien.riviere <donatien.riviere@f46b8487-d246-0410-87de-0759a80fe282>
2008-08-18 20:39:05 +0000

    right.

--- mp3lib/src/info/mp3lib/core/dao/LibraryDAO.java
+++ mp3lib/src/info/mp3lib/core/dao/LibraryDAO.java
@@ -4,6 +4,7 @@ import info.mp3lib.core.Album;
 import info.mp3lib.core.Library;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import org.apache.log4j.Logger;
@@ -12,63 +13,72 @@ import org.jdom.output.XMLOutputter;
 
 /**
  * Data Access Object for the <code>Library</code> class
+ * 
  * @author AkS
  */
 public class LibraryDAO {
-	
-	/** the unique instance of the singleton */
-	private static LibraryDAO instance;
-	
-	/** static access to the singleton */
-	public static LibraryDAO getInstance() {
-		if (instance == null) {
-			instance = new LibraryDAO();
-		}
-		return instance;
-	}
-	
-	/** Constructor */
-	private LibraryDAO() {
-	}
 
-	/** Apache log4j logger */
-	private final static Logger LOGGER = Logger.getLogger(Album.class.getName());
-	
-	public Library read(final String filePath) {
-		final Library result = null;
-		final File xmlFile = new File(filePath);
-		if (xmlFile.exists()) {
-			if (xmlFile.canRead()) {
-				// TODO check extension, implement
-			} else {
-				LOGGER.error(new StringBuffer("The given file [").append(filePath).append("] is not readable")
-						.toString());
-			}
-		} else {
-			LOGGER.error(new StringBuffer("The given file [").append(filePath).append("] does not exist")
-					.toString());
-		}
-		return result;
+    /** the unique instance of the singleton */
+    private static LibraryDAO instance;
+
+    /** static access to the singleton */
+    public static LibraryDAO getInstance() {
+	if (instance == null) {
+	    instance = new LibraryDAO();
 	}
-	
-	/**
-	 * Save the given library on the file system
-	 * @param lib the <code>Library</code> to persist
-	 * @filePath the absolute path of the file in which writing the XML tree
-	 * @return true if the given library is successfully persisted, false otherwise
-	 */
-	public boolean write(Library lib, final String filePath) {
-		boolean result = false;
-		  XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat()); //TODO set configurable
-		    try {
-		      outputter.output(lib.getRootElement(), System.out);
-		      result = true;
-		    }
-		    catch (IOException e) {
-		    	LOGGER.error(new StringBuffer("Unable to persist the library in the file [").append(filePath).append("]:\n")
-						.append(e.getMessage()).toString());
-		    }
-		    return result;
+	return instance;
+    }
+
+    /** Constructor */
+    private LibraryDAO() {
+    }
+
+    /** Apache log4j logger */
+    private final static Logger LOGGER = Logger.getLogger(Album.class.getName());
+
+    public Library read(final String filePath) {
+	final Library result = null;
+	final File xmlFile = new File(filePath);
+	if (xmlFile.exists()) {
+	    if (xmlFile.canRead()) {
+		// TODO check extension, implement
+	    } else {
+		LOGGER.error(new StringBuffer("The given file [").append(filePath)
+			.append("] is not readable").toString());
+	    }
+	} else {
+	    LOGGER.error(new StringBuffer("The given file [").append(filePath).append("] does not exist")
+		    .toString());
 	}
-	
+	return result;
+    }
+
+    /**
+     * Save the given library on the file system
+     * 
+     * @param lib
+     *                the <code>Library</code> to persist
+     * @filePath the absolute path of the file in which writing the XML tree
+     * @return true if the given library is successfully persisted, false
+     *         otherwise
+     */
+    public boolean write(Library lib, final String filePath) {
+	boolean result = false;
+	final File xmlFile = new File(filePath);
+	try {
+	    FileOutputStream fos = new FileOutputStream(xmlFile);
+	    XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat()); // TODO
+	    // set
+	    // configurable
+
+	    outputter.output(lib.getRootElement(), fos);
+	    result = true;
+	    fos.close();
+	} catch (IOException e) {
+	    LOGGER.error(new StringBuffer("Unable to persist the library in the file [").append(filePath)
+		    .append("]:\n").append(e.getMessage()).toString());
+	}
+	return result;
+    }
+
 }
