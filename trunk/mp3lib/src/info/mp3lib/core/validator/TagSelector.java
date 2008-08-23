1a02b78f418cec5c7aa3631a33ccd71842170ff3
palito686 <palito686@f46b8487-d246-0410-87de-0759a80fe282>
2008-08-23 16:20:41 +0000

    comment improved

--- trunk/mp3lib/src/info/mp3lib/core/validator/TagSelector.java
+++ trunk/mp3lib/src/info/mp3lib/core/validator/TagSelector.java
@@ -8,7 +8,7 @@ public interface TagSelector {
 	 * Retrieves the best tag query result matching the giving context and populate the given
 	 * result with it
 	 * @param context The context from witch selecting the best query
-	 * @param result the result to populate
+	 * @param result the result to populate, will be null if no query match the given context
 	 * @return an IQV denoting the level of match between the query result and the context
 	 */
 	public int selectTagsAlbum(final Context context, ITagQueryResult result);
