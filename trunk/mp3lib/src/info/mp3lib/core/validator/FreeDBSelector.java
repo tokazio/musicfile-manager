4b26eb0f86384979cf4fe10b6b734135c535aa43
palito686 <palito686@f46b8487-d246-0410-87de-0759a80fe282>
2008-08-23 16:14:22 +0000

    Selector component skeletton implemented attribute and element constants moved to the IXMLElement interface

---
+++ trunk/mp3lib/src/info/mp3lib/core/validator/FreeDBSelector.java
@@ -0,0 +1,15 @@
+package info.mp3lib.core.validator;
+
+import info.mp3lib.util.cddb.ITagQueryResult;
+
+public class FreeDBSelector implements TagSelector {
+
+	/**
+	 * @see TagSelector
+	 */
+	@Override
+	public int selectTagsAlbum(Context context, ITagQueryResult result) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
