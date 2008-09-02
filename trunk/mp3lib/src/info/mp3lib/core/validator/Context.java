d1c6822429dbdfe82544635172eee93979cf6af0
palito686 <palito686@f46b8487-d246-0410-87de-0759a80fe282>
2008-09-02 16:07:32 +0000

    Context squeleton improved, particularry for physical context

--- trunk/mp3lib/src/info/mp3lib/core/validator/Context.java
+++ trunk/mp3lib/src/info/mp3lib/core/validator/Context.java
@@ -2,19 +2,46 @@ package info.mp3lib.core.validator;
 
 public interface Context {
 
-    public int getArtistIQV();
+	/**
+	 * Retrieves the quality index of the artist name deduced from the context
+	 * @return the quality index for the artist name deduction
+	 */
+    public int getArtistQI();
     
-    public int getAlbumIQV();
+    /**
+	 * Retrieves the quality index of the album name deduced from the context
+	 * @return the quality index for the album name deduction
+	 */
+    public int getAlbumQI();
     
-    public int getTracksIQV();
+    /**
+	 * Retrieves the quality index of the tracks name deduced from the context
+	 * @return the quality index for the tracks name deduction
+	 */
+    public int getTracksQI();
     
+    /**
+     * @return the alnum name deduced from the context
+     */
     public String getAlbumName();
     
+    /**
+     * @return the artist name deduced from the context
+     */
     public String getArtistName();
     
+    /**
+     * @return the tracks name deduced from the context
+     */
     public String[] getTracksName();
     
+    /**
+     * @return the tracks length deduced from the context
+     */
     public int[] getTracksLength();
     
+    /**
+     * @return the tracks count
+     */
     public int getTracksCount();
 }
